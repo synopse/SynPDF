093fe488f649bc26f561626ebe10a7ac2184749f
Josh Kelley <joshkel@gmail.com>
2015-10-21 22:38:18 -0400

    C++Builder compatibility
    Use NODEFINE for macros, functions, and structs that are already defined in C++
    header files.
    
    Use NODEFINE to exclude TSynValidateText and its subclass, since C++Builder
    doesn't currently support array properties.
    
    Use "record" where possible, since C++Builder doesn't like "object."
    
    Add dummy parameters where necessary to constructor overloads (Delphi compiler
    warning W1029).
    
    See http://synopse.info/forum/viewtopic.php?pid=18151 for more info.

--- SynCommons.pas
+++ SynCommons.pas
@@ -8634,6 +8634,11 @@ type
     procedure SetParameters(Value: RawUTF8); override;
   end;
 
+  // C++Builder doesn't support array elements as properties (RSP-12595).
+  // For now, simply exclude the relevant classes from C++Builder.
+  {$NODEFINE TSynValidateText }
+  {$NODEFINE TSynValidatePassWord }
+
   /// will define a filter to be applied to a Record field content (typicaly
   // a TSQLRecord)
   // - a typical usage is to convert to lower or upper case, or
@@ -9853,7 +9858,7 @@ type
   {$A-}
   /// used to store a Date/Time in TSynTimeZone internal structures
   // - map Windows.TSystemTime, since it is how it is stored in the Registry
-  TTimeZoneValue = object
+  TTimeZoneValue = {$ifdef ISDELPHI2006ANDUP}record{$else}object{$ifend}
     wYear: Word;
     wMonth: Word;
     wDayOfWeek: Word;
@@ -9877,7 +9882,7 @@ type
   end;
   PTimeZoneInfo = ^TTimeZoneInfo;
   /// used to store Time Zone information for a single area in TSynTimeZone
-  TTimeZoneData = object
+  TTimeZoneData = {$ifdef ISDELPHI2006ANDUP}record{$else}object{$ifend}
     id: RawUTF8;
     display: RawUTF8;
     tzi: TTimeZoneInfo;
@@ -9918,7 +9923,8 @@ type
     // - under Linux, the file should be located with the executable, renamed
     // with a .tz extension - may have been created via SaveToFile(''), or
     // from a 'TSynTimeZone' bound resource
-    constructor CreateDefault;
+    // "dummy" parameter exists only to disambiguate constructors for C++
+    constructor CreateDefault(dummy: integer=0);
     /// finalize the instance
     destructor Destroy; override;
     {$ifdef MSWINDOWS}
@@ -27068,7 +27074,7 @@ begin
   fZones.InitSpecific(TypeInfo(TTimeZoneDataDynArray),fZone,djRawUTF8);
 end;
 
-constructor TSynTimeZone.CreateDefault;
+constructor TSynTimeZone.CreateDefault(dummy: integer);
 begin
   Create;
   {$ifdef LVCL}
